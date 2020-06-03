2f2fc70b1306727c3ee04035248d432d923089d6
余文锦 <windpenguin@gmail.com>
2020-06-03 14:56:31 +0800

    [feat] 构建前删除构建目录

--- Distrib.bat
+++ Distrib.bat
@@ -1,6 +1,11 @@
 @echo off
 
-if exist Build rd /q /s Build
+echo ÕýÔÚÉ¾³ýBuild
+:rm_build
+if exist Build rd Build /q /s
+timeout 1
+if exist Build goto rm_build
+
 mkdir Build
 cd Build
 cmake -DCMAKE_UTILITY_ROOT=E:\GitRepo\CMakeUtility -DJSONCPP_ROOT=G:\ThirdParty\jsoncpp\1.8.1\Visual_Studio_15_2017\Win64\MD\Debug ..
